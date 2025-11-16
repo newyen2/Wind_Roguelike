183084578454ce7f8489c0fe9fa1f7c110e43711
newyen2 <wish13love43xz@gmail.com>
2025-11-16 11:35:02 +0800

    DummyCard

--- Assets/Scripts/Cards/CardInstance.cs
+++ Assets/Scripts/Cards/CardInstance.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardInstance
+public class CardInstance: MonoBehaviour
 {
     // Start is called before the first frame update
     public CardData data;
 
     public int currentCost;
     public int currentWindPower;
-    public CardDirection direction;
+    public List<Direction> direction;
     public bool isExhausted;     // 用過就移出本局？
     public bool isTemporary;     // 戰鬥結束消失？
     public bool isUpgraded;      // 升級版？
@@ -19,6 +19,6 @@ public class CardInstance
         this.data = data;
         currentCost = data.baseCost;
         currentWindPower = data.baseWindPower;
-        direction = data.direction;
+        direction = data.directions;
     }
 }

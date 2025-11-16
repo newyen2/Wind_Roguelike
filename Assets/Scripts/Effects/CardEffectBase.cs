c2c381dcd23a2b6e99f7709e9a2d69c578d351ed
John1018 <sihua.john@gmail.com>
2025-11-16 12:55:11 +0800

    新增一些更方便的註解

--- Assets/Scripts/Effects/CardEffectBase.cs
+++ Assets/Scripts/Effects/CardEffectBase.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 public abstract class CardEffectBase : ScriptableObject
 {
-    public abstract void OnPlay(CardInstance self, EffectContext ctx);
-    public abstract void OnDraw(CardInstance self, EffectContext ctx);
-    public abstract void OnTurnEnd(CardInstance self, EffectContext ctx);
-    public abstract void OnDiscard(CardInstance self, EffectContext ctx);
+    public abstract void OnPlay(CardInstance self, EffectContext ctx);//見BuildWindManager.cs的trybuild
+    public abstract void OnDraw(CardInstance self, EffectContext ctx);//見DeckManager.cs的DrawCards
+    public abstract void OnTurnEnd(CardInstance self, EffectContext ctx);//見StageManager.cs的NextRound
+    public abstract void OnDiscard(CardInstance self, EffectContext ctx);//見DeckManager.cs的DiscardFromHand
 }

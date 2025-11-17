c5eff82a30764cc5649abcbf6aaa43420802237e
NEKOOAO <60414oscar@gmail.com>
2025-11-18 00:07:56 +0800

    add baka

--- Assets/Scripts/Building/cirno.cs
+++ Assets/Scripts/Building/cirno.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class cirno : BuildingBase
 {
+    public CardData cirnoCard;
+    CardInstance cirnoCardIns;
     public override int Score(int windPower, Direction windDirection, int x, int y,Wind wind)
     {
+        cirnoCardIns = new CardInstance(cirnoCard);
+        cirnoCardIns.isExhausted = true;
+        cirnoCardIns.isTemporary = true;
+
         this.multiplier = 1;
 
         if (StageManager.Instance.round != now_round)
@@ -16,10 +22,11 @@ public class cirno : BuildingBase
         {
             this.total_point = 99;
             AudioManager.Instance.Play("baka");
+            DeckManager.Instance.AddCardToDiscardPile(cirnoCardIns);
             return 99;
         }
 
-        return 0;
+        return 9;
     }
 }
 
